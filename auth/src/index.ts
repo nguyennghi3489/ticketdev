6c0fc005e0904e9a4c9aa0e0ee17aa3501ef2335
Nghi Nguyen <nguyennghi3489@gmail.com>
2023-10-17 18:54:42 +0700

    feat: integrate with mongo

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,7 @@ import { signinRouter } from "./router/signin";
 import { signupRouter } from "./router/signup";
 import { signoutRouter } from "./router/signout";
 import { errorHandler } from "./middleware/error-handler";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(json());
@@ -14,6 +15,18 @@ app.use(signoutRouter);
 app.use(signupRouter);
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("App is running in port 3000 !");
-});
+const start = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb://auth-mongo-srv:27017/auth?directConnection=true"
+    );
+    console.log("Connected to Mongo Database");
+  } catch (error) {
+    console.error(error);
+  }
+  app.listen(3000, () => {
+    console.log("App is running in port 3000 !");
+  });
+};
+
+start();
