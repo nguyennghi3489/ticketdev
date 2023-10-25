fb6fa09f4bf10aaa3ad3445e7362c2771348ee6b
Nghi Nguyen <nguyennghi3489@gmail.com>
2023-10-25 11:39:55 +0700

    feat: add tests for signin

---
+++ auth/src/router/__test__/signin.test.ts
@@ -0,0 +1,52 @@
+import request from "supertest";
+import { app } from "../../app";
+
+const TEST_EMAIL = "nnn2020@gmail.com";
+const TEST_PASS = "1923123";
+
+it("return 400 when a email does not exist is supplied", async () => {
+  await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: TEST_EMAIL,
+      password: TEST_PASS,
+    })
+    .expect(400);
+});
+
+it("return 400 when a password is not correct", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: TEST_EMAIL,
+      password: TEST_PASS,
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: TEST_EMAIL,
+      password: "wrongpassword",
+    })
+    .expect(400);
+});
+
+it("return 200 on signin successfully", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: TEST_EMAIL,
+      password: TEST_PASS,
+    })
+    .expect(201);
+
+  const response = await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: TEST_EMAIL,
+      password: TEST_PASS,
+    })
+    .expect(200);
+  expect(response.get("Set-Cookie")).toBeDefined();
+});
