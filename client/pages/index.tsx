f8fe1b2d2e59adc2b7fef5831521e8b35c290943
Nghi Nguyen <nguyennghi3489@gmail.com>
2023-10-28 17:57:10 +0700

    feat: fix problem when call api to service in service side

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,21 @@
-import { NextPage } from "next";
+import axios from "axios";
+import { NextPage, NextPageContext } from "next";
+import { AppContext } from "next/app";
 
 const Page: NextPage = () => {
   return <p>Landing Page</p>;
 };
 
+Page.getInitialProps = async (context: NextPageContext) => {
+  let response;
+  if (typeof window === "undefined") {
+    response = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      { headers: context.req.headers }
+    );
+  } else {
+    response = await axios.get("/api/users/currentuser");
+  }
+  return { data: response.data };
+};
 export default Page;
